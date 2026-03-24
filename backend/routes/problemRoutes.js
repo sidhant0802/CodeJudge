14cc3ce87b51cd64b2875c05e3f6f6b68d6e9897
sidhant <sidhantnirupam@gmail.com>
2026-03-24 16:14:35 +0530

    update

---
+++ backend/routes/problemRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const exampleController = require('../controllers/ProblemController.js');
+const auth = require('../middleware/auth.js');
+// router.get('/a', exampleController.a);
+router.get('/b',auth(["admin"]), exampleController.b);
+router.post('/create',auth(["admin"]), exampleController.create);
+// router.get('/read/:id',auth(["admin","user"]), exampleController.read);
+router.get('/read/:id',  exampleController.read);
+// router.get('/readall',auth(["admin","user"]), exampleController.readall);
+router.get('/readall',   exampleController.readall);
+// router.post('/login', exampleController.login);
+module.exports = router;
